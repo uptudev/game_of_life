a7286228df7fe91f13220a25d0743f332f36d567
uptu <bh1000@live.com>
2023-02-17 00:49:06 -0700

    Refactored into a WGSL shader which runs well.

--- game_libs/src/resources/mod.rs
+++
@@ -1,5 +0,0 @@
-pub use map_options::*;
-
-mod map_options;
-pub(crate) mod tile;
-pub mod tile_map;
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,31 @@
 use bevy::{prelude::*, DefaultPlugins};
-#[allow(unused_imports)]
-use game_libs::{GameOfLife, resources::MapOptions};
+
+mod compute_shader;
+
+use compute_shader::{GameOfLifeComputePlugin, setup};
 
 #[cfg(feature = "debug")]
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
-#[derive(Resource, Default, Clone)]
-#[allow(dead_code)]
-pub struct WindowDescriptor {
-    title: String,
-    width: usize,
-    height: usize,
-}
-
 fn main() {
     let mut app = App::new();
-    // Window setup
-    app.insert_resource(WindowDescriptor {
-        title: "Mine Sweeper!".to_string(),
-        width: 700,
-        height: 800,
-        ..Default::default()
-    }).insert_resource(MapOptions {
-            map_size: (100, 100),
-            alive_count: 5000,
-            tile_padding: 3.0,
+    app.insert_resource(ClearColor(Color::BLACK))
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+        window: WindowDescriptor {
+            // uncomment for unthrottled FPS
+            // present_mode: bevy::window::PresentMode::AutoNoVsync,
+            title: "Game of Life".to_string(),
+            width: 1920.0,
+            height: 1080.0,
             ..Default::default()
-        })
-        .add_plugins(DefaultPlugins)
-        .add_system(bevy::window::close_on_esc)
-        .add_plugin(GameOfLife);
+        },
+        ..default()
+    }));
+    // Window setup
+    app.add_system(bevy::window::close_on_esc)
+        .add_plugin(GameOfLifeComputePlugin)
+        .add_startup_system(setup);
     #[cfg(feature = "debug")]
     app.add_plugin(WorldInspectorPlugin);
-    app.add_startup_system(camera_setup);
     app.run();
-}
-
-fn camera_setup(mut commands: Commands) {
-    // 2D orthographic camera
-    commands.spawn(Camera2dBundle::default());
 }
